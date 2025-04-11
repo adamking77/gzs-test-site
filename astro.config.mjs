3266c3178fff6343ff544f2c680d1f5e097cba7e
Adam King <adamking@GenZen-Air.local>
2025-04-11 08:33:40 +0400

    Add GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  // Add your domain name here
-  site: "https://yourwebsite.com/",
+  integrations: [tailwind()],
+  site: 'https://adamking77.github.io',
+  base: '/genzen-solutions',
   integrations: [ sitemap()]
 });
