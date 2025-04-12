31ba3dbaf0ebe4b075a74c95bb3320c0044c9213
Adam King <adamking@GenZen-Air.local>
2025-04-12 20:56:43 +0400

    Fix: Update base URL and links for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), alpinejs()],
   site: 'https://adamking77.github.io',
   base: '/genzen-solutions',
   build: {
